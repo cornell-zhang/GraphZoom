6304685410a0bed082a2510b87baa4fecdfe3e68
Anonymous <>
2020-01-03 18:08:46 -0500

    remove base

--- graphzoom/cora.sh
+++ graphzoom/cora.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-for i in `seq 1 5`    ## coarsening level
+for i in `seq 1 4`    ## coarsening level
     do
         ratio=$(case "$i" in
-            (1)  echo 2;; 
+            (1)  echo 2;;
             (2)  echo 5;;
             (3)  echo 9;;
             (4)  echo 19;;
-            (5)  echo 27;;
         esac)
         python graphzoom.py -r ${ratio} -m deepwalk
 done
